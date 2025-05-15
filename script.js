d26089da732dfdb50c4bd47aee11a54a7f2637ee
foxtrot012 <moussakamissoko078@gmail.com>
2025-05-15 06:01:16 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector('.toggle_btn');
+var nav = document.querySelector('.onglet_mobile');
+
+btn.onclick = function(){
+  .onglet_mobile.classList.toggle('.onglet_mobile_open');
+}
