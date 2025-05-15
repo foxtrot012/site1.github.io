f50742352a988858444d7e5e5af4c6ee68c2af8a
foxtrot012 <moussakamissoko078@gmail.com>
2025-05-15 06:17:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var btn = document.querySelector('.toggle_btn');
 var nav = document.querySelector('.onglet_mobile');
 
 btn.onclick = function(){
-  nav.classList.toggle('.onglet_mobile_open');
+  nav.classList.toggle('onglet_mobile_open');
 }
